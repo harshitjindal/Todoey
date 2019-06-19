40a72f6715d0e5988c9f5b3d0b9b8069a7afc993
harshitjindal <harshit.jindal@icloud.com>
2019-06-19 18:18:34 +0530

    UI Changes

--- Todoey/View/CategoryViewController.swift
+++ Todoey/View/CategoryViewController.swift
@@ -16,10 +16,11 @@ class CategoryViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
